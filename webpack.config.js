e666f564a80585611ab26fdbdec079e6219a9c62
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-01-12 13:41:16 -0800

    bundle.js file created

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	entry: './client/src/index.jsx',
 	output: {
-		filename: bundle.js,
+		filename: 'bundle.js',
 		path: path.resolve(__dirname, './client/dist')
 	},
 	module:{

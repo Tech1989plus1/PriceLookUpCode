1f41b4e0275aeec6606e719a4c98aeac65d9d828
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-01-12 14:22:35 -0800

    App.jsx file added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => console.log(`Listen on port: ${port}!`));

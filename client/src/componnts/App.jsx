1f705b37ff356d749fde07d83e22da933087da6e
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-01-13 11:45:36 -0800

    next working on creating fake data

--- client/src/componnts/App.jsx
+++ client/src/componnts/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from './Title.jsx';
+import Recent from './Recent.jsx';
 
 class App extends React.Component{
   constructor(props){
     super(props);
 
     this.state = {
+      view: 'home',
     }
     this.search = this.search.bind(this);
   }
@@ -14,7 +16,17 @@ class App extends React.Component{
     console.log(item);
   }
 
-  componentDidMount() {
+  renderView(){
+    const { view } = this.state;
+    
+    if (view === 'home') {
+      return (
+        <Recent/>
+      );
+    }
+  }
+
+  componentDidMount(){
     
   }
 
@@ -22,6 +34,9 @@ class App extends React.Component{
     return(
       <div>
         <Title title={'Plu.Bakery.Orange'} searchCB={this.search}/>
+        <div className='container-body'>
+          {this.renderView()}
+        </div>
       </div>);
   }
 }
---
+++ client/src/componnts/Recent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Recent = (props) => {
+  return(<div>
+      <h1>hello</h1>
+    </div>);
+};
+
+export default Recent;
